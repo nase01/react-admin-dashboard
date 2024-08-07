a1f8e2594a9fc52a0c42d39f0deeb3645a08af5e
Sony <nasesony01@gmail.com>
2024-08-07 16:23:05 +0800

    Refactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import { LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { useSignOut } from "@/lib/react-query/queries";
 import { useUserContext, INITIAL_USER } from "@/context/AuthContext";
+import { NavLink } from "@/types";
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const { setUser, setIsAuthenticated } = useUserContext();
   const { mutate: signOut } = useSignOut();
 
@@ -20,12 +22,25 @@ const Header = () => {
     navigate("/sign-in");
   };
 
+  const links: NavLink[] = [
+    { route: "/admin/dashboard", label: "Dashboard" },
+    { route: "/admin/users", label: "Users" },
+  ];
+
   return (
     <div className="flex justify-between items-center p-4 bg-slate-950 text-white">
       <h1 className="text-xl font-bold">RAD</h1>
       <ul className="flex-grow flex justify-center space-x-4 text-white">
-        <li><a href="/admin/dashboard" className="hover:underline">Dashboard</a></li>
-        <li><a href="/admin/users" className="hover:underline">Users</a></li>
+        {links.map((link) => (
+          <li key={link.route}>
+            <a
+              href={link.route}
+              className={`hover:underline ${location.pathname === link.route ? "underline" : ""}`}
+            >
+              {link.label}
+            </a>
+          </li>
+        ))}
       </ul>
       <Button onClick={(e) => handleSignOut(e)} size="icon" className="rounded-full">
         <LogOut className="w-5 h-5" />
