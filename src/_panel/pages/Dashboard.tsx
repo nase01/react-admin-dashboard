e3b557690e0ce6885e4b8440569c25f3a410a234
Sony <nasesony01@gmail.com>
2024-08-07 16:06:19 +0800

    Fetch currentUser

--- src/_panel/pages/Dashboard.tsx
+++ src/_panel/pages/Dashboard.tsx
@@ -1,7 +1,19 @@
+
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queries";
+
 const Dashboard = () => {
+  const { data: currentUser } = useGetCurrentUser();
+  
+  if (!currentUser)
+    return (
+      <Loader />
+    );
+
   return (
     <div className="p-4">
-      <h2 className="font-bold">Dashboard</h2>
+      <h2 className="font-bold text-slate-900 text-2xl">Dashboard</h2>
+      <div className="mt-3">Welcome {currentUser.name} ({currentUser.role})</div>
     </div>
   )
 }
