a50d7dd48c0a226fba2abe21ce0a8af63004cffd
Sony <nasesony01@gmail.com>
2024-08-08 15:37:07 +0800

    Implement useGetAllUsers

--- src/_panel/pages/Users.tsx
+++ src/_panel/pages/Users.tsx
@@ -1,7 +1,28 @@
+
+import Loader from "@/components/shared/Loader";
+import { useGetAllUsers } from "@/lib/react-query/queries";
+import { User } from "@/types"
+
 const Users = () => {
+  const { data: usersData, isLoading } = useGetAllUsers();
+
+  if (isLoading)
+    return (
+      <Loader />
+    );
+
+  const users = usersData as User[];
+
   return (
     <div className="p-4">
-      <h2 className="font-bold" >Users</h2>
+      <h2 className="font-bold text-slate-900 text-2xl">Users</h2>
+      <div className="mt-3">
+        {users?.map((user: User) => (
+          <div key={user.id} className="user-item">
+            * {user.name}
+          </div>
+        )) || "Failed to Fetch or Unauthorized"}
+      </div>
     </div>
   )
 }
