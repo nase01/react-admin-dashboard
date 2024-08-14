a119448eb539696b6257655547e941581b23bb76
Sony <nasesony01@gmail.com>
2024-08-14 16:27:10 +0800

    Modify AuthLayout responsive style

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout() {
     <>
       <section className="flex flex-1 justify-center items-center flex-col py-10">
         <div className="flex justify-between rounded-[30px] overflow-hidden p-5">
-          <div className="flex-center flex-col p-3 sm:w-420 min-w-[450px]">
+          <div className="flex-center flex-col p-3 md:w-[450px]">
             <div className="flex justify-center items-center">
               <img src="/assets/logo.svg" alt="logo" />
             </div>
