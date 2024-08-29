a558a078efd89386809b8410f3f6d7987316db58
Sony <nasesony01@gmail.com>
2024-08-29 15:17:54 +0800

    Render UserForm component

--- src/_panel/pages/AccountSettings.tsx
+++ src/_panel/pages/AccountSettings.tsx
@@ -1,6 +1,22 @@
+import UserForm from "../forms/UserForm"
+import { useGetCurrentUser } from "@/lib/react-query/queries";
+import Loader from "@/components/shared/Loader";
+
 const AccountSettings = () => {
+  const { data: currentUser } = useGetCurrentUser();
+
+	if (!currentUser) return <Loader />;
+
   return (
-    <div>AccountSettings</div>
+    <div className="p-4">
+      <h2 className="font-bold text-slate-900 text-2xl">Account Settings</h2>
+      <div className="mt-3 max-w-[500px]">
+				{ currentUser 
+          ? <UserForm  userId={currentUser.id} userData={currentUser} ownAccount /> 
+          : "Error: Failed to fetch user data." 
+        }
+      </div>
+    </div>
   )
 }
 
