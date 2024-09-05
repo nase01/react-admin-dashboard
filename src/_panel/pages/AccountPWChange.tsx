fa4e56c2026058e49184b6995d4a0173ab74de3a
Sony <nasesony01@gmail.com>
2024-09-05 15:26:47 +0800

    Add warning message for force password change

--- src/_panel/pages/AccountPWChange.tsx
+++ src/_panel/pages/AccountPWChange.tsx
@@ -10,8 +10,15 @@ const AccountPWChange = () => {
 	return (
     <div className="p-4">
       <h2 className="font-bold text-slate-900 text-2xl">Password Change</h2>
-      <div className="mt-3 max-w-[500px]">
-				{ currentUser 
+      <div className="mt-3 max-w-[550px]">  
+      
+        {currentUser && currentUser.pwForceChange && (
+          <div className="mt-2 p-3 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700">
+            You are required to change your password to continue using this panel.
+          </div>
+        )}
+
+        { currentUser 
           ? <ChangePWForm  /> 
           : "Error: Failed to fetch user data." 
         }
