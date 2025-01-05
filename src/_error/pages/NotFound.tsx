dbdaa889cb95e062c7462d1bf7eec237c007e2c1
Sony <nasesony01@gmail.com>
2025-01-05 16:45:10 +0800

    Change back link logic

--- src/_error/pages/NotFound.tsx
+++ src/_error/pages/NotFound.tsx
@@ -3,10 +3,12 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeftCircle } from "lucide-react";
 import { useEffect } from "react";
 import { updatePageTitle } from "@/lib/utils";
+import { getJwt } from '@/lib/utils';
 
 const Unauthorized = () => {
 	const navigate = useNavigate();
 	const location = useLocation();
+	const jwt = getJwt();
 
   const handleBackToDashboard = () => {
     navigate('/panel/dashboard');
@@ -21,8 +23,8 @@ const Unauthorized = () => {
 			<h2 className="mb-5 text-2xl font-extrabold text-danger">404 - Not Found</h2>
 			It seems the page <b>"{location.pathname}"</b> you want to access  does not exist.
 			<div className="mt-10">
-				<Button size="sm" onClick={handleBackToDashboard}>
-					<ArrowLeftCircle className="mr-2" size={16} /> Back to Dashboard
+				<Button size="sm" onClick={() => jwt ? handleBackToDashboard :  window.history.back()}>
+					<ArrowLeftCircle className="mr-2" size={16} /> Back {jwt && "to Dashboard" }
 				</Button>
 			</div>
 		</div>
